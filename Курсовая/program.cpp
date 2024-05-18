48e533fd5a952f6c526c0e15ac74975a0c08d97f
Abu9541 <144372138+Abu9541@users.noreply.github.com>
2024-05-18 16:56:45 +0300

    Update program.cpp

--- Курсовая/program.cpp
+++ Курсовая/program.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-#include <list>
 #include "program.h"
 
 using namespace std;
@@ -16,10 +14,13 @@ int main(){
     do {
         cout << "\nМеню\nВыберите функцию\n";
         cout << "1. Сформировать начальные данные вводом с клавиатуры\n";
-        cout << "2. Выезд автобуса из парка\n";
-        cout << "3. Въезд автобуса в парк\n";
-        cout << "4. Вывод информации об автобусах\n";
-        cout << "5. Выход из программы\n";
+        cout << "2. Сформировать начальные данные вводом с файла\n";
+        cout << "3. Выезд автобуса из парка\n";
+        cout << "4. Въезд автобуса в парк\n";
+        cout << "5. Вывод информации об автобусах в консоль\n";
+        cout << "6. Вывод информации об автобусах в файл\n";
+        cout << "7. Удалить данные из массивов\n";
+        cout << "8. Выход из программы\n";
         cout << "Введите ваш выбор: ";
         cin >> choice;
         cout << endl;
@@ -29,21 +30,31 @@ int main(){
                 park.make_start_data();
                 break;
             case 2:
-                park.departure();
+                park.make_start_data_from_file();
                 break;
             case 3:
-                park.entry();
+                park.departure();
                 break;
             case 4:
-                park.display_data();
+                park.entry();
                 break;
             case 5:
+                park.display_data();
+                break;
+            case 6:
+                park.display_data_in_file();
+                break;
+            case 7:
+                park.delete_data();
+                break;
+            case 8:
                 cout << "Выход из программы...\n";
                 break;
             default:
                 cout << "Неверный выбор. Пожалуйста, введите существующий вариант.\n";
         }
-    } while (choice != 5);
+    } while (choice != 8);
 
     return 0;
 }
+
